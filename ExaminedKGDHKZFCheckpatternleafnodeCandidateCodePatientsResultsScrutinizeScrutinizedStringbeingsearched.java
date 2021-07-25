500dbdd2646c133ac1b07f0f85c2a21b8baa6d57
Saif Ahmed <40402050+Saifahmedprogrminlocallang@users.noreply.github.com>
2021-07-25 12:47:18 +0530

    Add files via upload

---
+++ ExaminedKGDHKZFCheckpatternleafnodeCandidateCodePatientsResultsScrutinizeScrutinizedStringbeingsearched.java
@@ -0,0 +1,325 @@
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+
+
+public class ExaminedKGDHKZFCheckpatternleafnodeCandidateCodePatientsResultsScrutinizeScrutinizedStringbeingsearched{
+   public static void main(String args[]){
+
+	String Virusunderscrutiny="coronavirus";
+
+	  String jleaferptchbloodsampleavailableInputLangKeywordFilename = "D:\\Saif\\LocallangProgram\\Test\\Inputtestfiletobescrutinizedforpatientresults.dat";
+
+	      String strCurrentLinenew2 = "";
+	      String keyword2=""; 
+		  String keywordstartpattern2 = "-";
+		  
+		  int keywordhyphenstartpattern2 =0;
+		  
+		  int counter2 =0;
+		int Checkleafsamplematchcoronavirus;
+		Checkleafsamplematchcoronavirus=0;	
+		int countlenkeywordhyphenstartpattern2len;
+		countlenkeywordhyphenstartpattern2len=0;	  
+	      int zkj;
+	      zkj=0;
+	      String fstringkeyword2;
+	       fstringkeyword2="";
+
+int Checkleafsamplematchcoronaviruspattern;
+Checkleafsamplematchcoronavirus=0;
+		  
+	      try {
+
+			  BufferedReader brcheckmusicpattern_inputlanglkfile = new BufferedReader(new FileReader(jleaferptchbloodsampleavailableInputLangKeywordFilename));
+					  
+			    while ((strCurrentLinenew2=brcheckmusicpattern_inputlanglkfile.readLine()) != null) {
+
+				if (zkj<=2) {
+			        System.out.println("strCurrentLinenew2 " + strCurrentLinenew2);
+			    	
+			        keywordhyphenstartpattern2 = strCurrentLinenew2.indexOf(keywordstartpattern2);
+				countlenkeywordhyphenstartpattern2len=strCurrentLinenew2.length();
+			    	if (keywordhyphenstartpattern2>0) {
+			        keyword2 = strCurrentLinenew2.substring(keywordhyphenstartpattern2+2,countlenkeywordhyphenstartpattern2len);
+			    	//keyword2 = keyword2.substring(4, keyword2.length());
+			    	keyword2 = keyword2.trim();
+				fstringkeyword2=fstringkeyword2+keyword2;
+			    	/**
+				ht2.put(counter2, ""+keyword2);
+				hm2.put(counter2, ""+counter2);
+				**/
+			    	System.out.println("keyword2 - " +keyword2);
+
+			    	counter2 = counter2 + 1;
+				}
+				zkj=zkj+1;
+			    	}
+			    }
+			    brcheckmusicpattern_inputlanglkfile.close();
+			   } catch (IOException ZKTDTKiznkioe) {
+				ZKTDTKiznkioe.getStackTrace();   
+			   } catch (Exception e) {
+				e.getStackTrace();   
+			   } 		
+
+
+String BloodCompositionofpatients[]=new String[10];
+
+int zijk;
+zijk=0;
+int numberofpatients;
+numberofpatients=0;
+
+
+String args1[]={"1","2","3"};
+String args2[]={"abcde","crnas","onarous"};
+
+
+
+Virusunderscrutiny=args1[0];
+args1[0]="pcjeptilkhbsfqijgecoronavirus";
+args1[1]="bpcjeptilkhbsfqijgecoronavirus";
+//args2[1]="abcdse";
+//args2[2]="crnas";
+
+String fStringbeingsearched;
+fStringbeingsearched="";
+fStringbeingsearched=args2[1];
+
+Virusunderscrutiny="bpcjeptilkhbsfqijgecoronavirus";
+
+//Virusunderscrutiny=args1[1];
+
+int hjinzpatientnumber=0;
+
+String nzpatientnumberstring="";
+
+try {
+/**
+nzpatientnumberstring = args1[0];
+**/
+/**
+nzpatientnumberstring=fStringbeingsearched;
+**/
+
+nzpatientnumberstring=args2[1];
+hjinzpatientnumber= 1;
+
+} catch(NumberFormatException ZKTDTKNFEbNFEiznkioe) {
+
+ZKTDTKNFEbNFEiznkioe.getStackTrace();
+} catch (Exception e) {
+e.getStackTrace();
+}
+
+int z;
+z=0;
+
+int lycydsknjgtrck;
+lycydsknjgtrck=0;
+int PatientBloodSequenceofPatienttobeinvestigatedSetFlagfortheSequenceFlagSetResult[]= new int[lycydsknjgtrck];
+int zk;
+zk=0;
+for(zk=0;zk<lycydsknjgtrck;zk++){
+PatientBloodSequenceofPatienttobeinvestigatedSetFlagfortheSequenceFlagSetResult[zk]=0;
+}
+
+int ijknz=0;
+
+
+int i;
+i=0;
+
+
+
+String BloodSequenceofpatients[]=new String[3];
+
+//get the order of bit endian of the blood infected by virus and blood sequence
+
+while (i<=2) {
+BloodSequenceofpatients[i] = args2[i];
+i++;
+}
+
+int patientBloodsequencelength=0;
+int chamkinodepolkaklm;
+chamkinodepolkaklm=0;
+
+int shirtnodepolkaklm;
+shirtnodepolkaklm=1;
+
+char dfhgsf;
+dfhgsf='b';
+
+String Stringindexofbloodcompositionsequencestringinviruscompositionstringcheckedstring;
+Stringindexofbloodcompositionsequencestringinviruscompositionstringcheckedstring="";
+
+int BloodSequenceFlagSetPresent[]= new int[patientBloodsequencelength];
+
+String PatientBloodSequenceofPatienttobeinvestigatedSequence;
+PatientBloodSequenceofPatienttobeinvestigatedSequence="";
+PatientBloodSequenceofPatienttobeinvestigatedSequence=BloodSequenceofpatients[2];
+patientBloodsequencelength=PatientBloodSequenceofPatienttobeinvestigatedSequence.length();
+
+lycydsknjgtrck=PatientBloodSequenceofPatienttobeinvestigatedSequence.length();
+
+
+String PatientBloodSequenceofPatienttobeinvestigatedSetFlagfortheSequence="";
+
+//for (chamkinodepolkaklm=0;chamkinodepolkaklm<hjinzpatientnumber;chamkinodepolkaklm++) {
+
+patientBloodsequencelength = BloodSequenceofpatients[1].length();
+
+
+int knzbloodsampleavailable;
+knzbloodsampleavailable=PatientBloodSequenceofPatienttobeinvestigatedSequence.length();
+
+PatientBloodSequenceofPatienttobeinvestigatedSetFlagfortheSequence=args1[0];
+int zknjh;
+zknjh=0;
+
+int PatientBloodSequenceofPatienttobeinvestigatedSetFlagfortheSequencebool=0;
+String Stringindexofbloodcompositionsequencestringinviruscompositionstring;
+Stringindexofbloodcompositionsequencestringinviruscompositionstring="";
+int ixdjzkt;
+
+ixdjzkt=0;
+
+int jzkjhijpt;
+jzkjhijpt=0;
+
+int yquvcjkpt;
+yquvcjkpt=0;
+
+int mhytpuim;
+mhytpuim=0;
+
+String keyword2calculatedvirussequencetobesearchedandexamined;
+keyword2calculatedvirussequencetobesearchedandexamined="";
+
+int cvedsihgpkfoundkeyword2calculatedvirussequencetobesearchedandexamined;
+cvedsihgpkfoundkeyword2calculatedvirussequencetobesearchedandexamined=0;
+z=0;
+
+int cvedsihgpkfoundStringindexofbloodcompositionsequencestringinviruscompositionstring;
+cvedsihgpkfoundStringindexofbloodcompositionsequencestringinviruscompositionstring=0;
+
+int searcheddhjzStringindexofbloodcompositionsequencestringinviruscompositionstring;
+searcheddhjzStringindexofbloodcompositionsequencestringinviruscompositionstring=0;
+
+for(zknjh=0;zknjh<knzbloodsampleavailable;zknjh++) {
+
+
+String PatientBloodSequenceofPatienttobestringtobesearchedthatwhichisavailable = "";
+PatientBloodSequenceofPatienttobestringtobesearchedthatwhichisavailable = PatientBloodSequenceofPatienttobeinvestigatedSequence.charAt(zknjh)+"";
+cvedsihgpkfoundStringindexofbloodcompositionsequencestringinviruscompositionstring = fStringbeingsearched.indexOf(PatientBloodSequenceofPatienttobestringtobesearchedthatwhichisavailable);
+
+searcheddhjzStringindexofbloodcompositionsequencestringinviruscompositionstring=yquvcjkpt;
+dfhgsf=Virusunderscrutiny.charAt(yquvcjkpt);
+
+Stringindexofbloodcompositionsequencestringinviruscompositionstringcheckedstring= dfhgsf+"";
+
+
+if (yquvcjkpt==cvedsihgpkfoundStringindexofbloodcompositionsequencestringinviruscompositionstring){
+z=z+1;
+
+mhytpuim=1;
+ixdjzkt=ixdjzkt+1;
+jzkjhijpt=jzkjhijpt+1;
+yquvcjkpt=yquvcjkpt+1;
+}
+else {
+System.out.println("sequence not matched");
+}
+
+//
+/**if (mhytpuim==1){
+continue;
+}**/
+
+
+}
+
+int nkz=0;
+int sczknjhs;
+sczknjhs=1;
+sczknjhs=yquvcjkpt;
+
+int qbknpioyg;
+qbknpioyg=0;
+
+for(nkz=0;nkz<=sczknjhs;nkz++) {
+   if (cvedsihgpkfoundkeyword2calculatedvirussequencetobesearchedandexamined<=searcheddhjzStringindexofbloodcompositionsequencestringinviruscompositionstring){
+   //set blood sample ascending order normalized checked flag  PatientBloodSequenceofPatienttobeinvestigatedSetFlagfortheSequencebool=0;
+   PatientBloodSequenceofPatienttobeinvestigatedSetFlagfortheSequencebool=0;
+   qbknpioyg=qbknpioyg+1;     
+   } else {
+   PatientBloodSequenceofPatienttobeinvestigatedSetFlagfortheSequencebool=1;
+}     
+
+
+System.out.println("qbknpioyg-"+qbknpioyg);
+
+
+lycydsknjgtrck=PatientBloodSequenceofPatienttobeinvestigatedSequence.length();
+
+ijknz=0;
+
+while (ijknz<PatientBloodSequenceofPatienttobeinvestigatedSequence.length()){
+if (PatientBloodSequenceofPatienttobeinvestigatedSetFlagfortheSequencebool==1) {
+PatientBloodSequenceofPatienttobeinvestigatedSetFlagfortheSequenceFlagSetResult[ijknz]= 1;
+
+ijknz=ijknz+1;
+Checkleafsamplematchcoronavirus++;
+
+} else {
+if(ijknz==PatientBloodSequenceofPatienttobeinvestigatedSequence.length()){
+ijknz=ijknz-1;
+break;
+}
+PatientBloodSequenceofPatienttobeinvestigatedSetFlagfortheSequenceFlagSetResult[ijknz]= 0;
+
+ijknz=ijknz+1;
+}
+
+
+}
+}
+
+ijknz=ijknz-1;
+
+int dkzhji;
+dkzhji=1;
+ijknz=0;
+
+while(dkzhji<=PatientBloodSequenceofPatienttobeinvestigatedSequence.length()) {
+if(PatientBloodSequenceofPatienttobeinvestigatedSetFlagfortheSequenceFlagSetResult[ijknz]==1) {
+   System.out.println("Result - "+PatientBloodSequenceofPatienttobeinvestigatedSetFlagfortheSequenceFlagSetResult[ijknz]+" - kgihtptrittptk ...- sequence str str found");
+} else {
+   System.out.println("Result - "+PatientBloodSequenceofPatienttobeinvestigatedSetFlagfortheSequenceFlagSetResult[ijknz]+" - ytkgihfailure ...- sequence str str not found");
+}
+dkzhji=dkzhji+1;
+ijknz=ijknz+1;
+
+}
+
+if (Checkleafsamplematchcoronavirus==(nzpatientnumberstring.length())){
+   System.out.println("Result - "+PatientBloodSequenceofPatienttobeinvestigatedSetFlagfortheSequenceFlagSetResult[ijknz]+" - coronavirus matched. blood sample positive. kgihtptrittptk ...- sequence str str found");
+}
+
+shirtnodepolkaklm=shirtnodepolkaklm+1;
+PatientBloodSequenceofPatienttobeinvestigatedSequence=BloodSequenceofpatients[shirtnodepolkaklm];
+
+
+
+
+}
+
+
+
+
+
+}
+
+
